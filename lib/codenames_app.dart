e4b6d41a6726bf26aeaef9a91664c29658b0f49f
Zoey Child <imperialoctopus@gmail.com>
2020-10-05 14:16:57 +0100

    Update appview

--- lib/codenames_app.dart
+++ lib/codenames_app.dart
@@ -1,7 +1,9 @@
-import 'package:codenames/bloc/game/game_bloc.dart';
-import 'package:codenames/service/new_game_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/game/game_bloc.dart';
+import 'screen/menu_screen.dart';
+import 'service/new_game_service.dart';
 
 class CodenamesApp extends StatelessWidget {
   const CodenamesApp({
@@ -38,30 +40,7 @@ class _AppViewState extends State<AppView> {
   Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: _navigatorKey,
-      builder: (context, child) {
-        return BlocListener<AuthenticationBloc, AuthenticationState>(
-          listener: (context, state) {
-            switch (state.status) {
-              case AuthenticationStatus.authenticated:
-                _navigator.pushAndRemoveUntil<void>(
-                  HomePage.route(),
-                  (route) => false,
-                );
-                break;
-              case AuthenticationStatus.unauthenticated:
-                _navigator.pushAndRemoveUntil<void>(
-                  LoginPage.route(),
-                  (route) => false,
-                );
-                break;
-              default:
-                break;
-            }
-          },
-          child: child,
-        );
-      },
-      onGenerateRoute: (_) => SplashPage.route(),
+      home: MenuScreen(),
     );
   }
 }
