50ab4fe9fecfb4e0f3edf721397e1f71bc7966b9
Zoey Child <imperialoctopus@gmail.com>
2020-10-06 16:15:07 +0100

    Add fontawesome icons

--- lib/screen/play_screen.dart
+++ lib/screen/play_screen.dart
@@ -1,3 +1,6 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../model/card_affiliation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -8,6 +11,10 @@ import '../model/code_card.dart';
 
 class PlayScreen extends StatelessWidget {
   static const cardColor = Color.fromARGB(255, 245, 230, 200);
+  static const redColor = Colors.red;
+  static const blueColor = Colors.blue;
+  static const assassinColor = Colors.black;
+  static const iconColor = Color.fromARGB(100, 0, 0, 0);
   static const backButtonColor = Color.fromARGB(255, 240, 30, 30);
 
   @override
@@ -32,9 +39,16 @@ class PlayScreen extends StatelessWidget {
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('No game data found'),
-        RaisedButton(
-            child: Text('Back'), onPressed: () => Navigator.of(context).pop()),
+        Text(
+          'no game data found',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: OutlinedButton(
+              child: Text('back'),
+              onPressed: () => Navigator.of(context).pop()),
+        ),
       ],
     ));
   }
@@ -118,18 +132,94 @@ class PlayScreen extends StatelessWidget {
   Widget _cardBuilder(
       BuildContext context, GameStatePlaying state, CodeCard card) {
     if (card.visible) {
-      return Expanded(
-        child: Card(
-          color: cardColor,
-          child: InkWell(
-            onTap: () {
-              BlocProvider.of<GameBloc>(context)
-                  .add(GameEventFlip(state.cards.indexOf(card)));
-            },
-            child: Text(card.affiliation.toString()),
-          ),
-        ),
-      );
+      switch (card.affiliation) {
+        case CardAffiliation.neutral:
+          return Expanded(
+            child: Card(
+              color: cardColor,
+              child: InkWell(
+                onTap: () {
+                  BlocProvider.of<GameBloc>(context)
+                      .add(GameEventFlip(state.cards.indexOf(card)));
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child:
+                        FittedBox(child: Icon(Icons.close, color: iconColor)),
+                  ),
+                ),
+              ),
+            ),
+          );
+        case CardAffiliation.red:
+          return Expanded(
+            child: Card(
+              color: redColor,
+              child: InkWell(
+                onTap: () {
+                  BlocProvider.of<GameBloc>(context)
+                      .add(GameEventFlip(state.cards.indexOf(card)));
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: FittedBox(
+                        child: FaIcon(FontAwesomeIcons.userSecret,
+                            color: iconColor)),
+                  ),
+                ),
+              ),
+            ),
+          );
+        case CardAffiliation.blue:
+          return Expanded(
+            child: Card(
+              color: blueColor,
+              child: InkWell(
+                onTap: () {
+                  BlocProvider.of<GameBloc>(context)
+                      .add(GameEventFlip(state.cards.indexOf(card)));
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: FittedBox(
+                        child: FaIcon(FontAwesomeIcons.userSecret,
+                            color: iconColor)),
+                  ),
+                ),
+              ),
+            ),
+          );
+        case CardAffiliation.assassin:
+          return Expanded(
+            child: Card(
+              color: assassinColor,
+              child: InkWell(
+                onTap: () {
+                  BlocProvider.of<GameBloc>(context)
+                      .add(GameEventFlip(state.cards.indexOf(card)));
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: FittedBox(
+                      child:
+                          FaIcon(FontAwesomeIcons.skull, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        default:
+          throw FallThroughError();
+      }
     } else {
       return Expanded(
         child: Card(
