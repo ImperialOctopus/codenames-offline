79042b944a66a1e6678cfcd8e416017b44c566d8
Zoey Child <imperialoctopus@gmail.com>
2020-10-06 19:13:06 +0100

    Add custom theme colours

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
-final defaultTheme = ThemeData.light();
+final themeData = ThemeData(
+  // Define the default brightness and colors.
+  brightness: Brightness.light,
+  primaryColor: Color.fromARGB(255, 110, 20, 69),
+  accentColor: Color.fromARGB(255, 181, 51, 70),
 
-final themeData = defaultTheme.copyWith(
   outlinedButtonTheme: OutlinedButtonThemeData(
     style: OutlinedButton.styleFrom(
       textStyle: TextStyle(fontSize: 32),
