6c5e2b98caf62b8cc1bbfb8b74bd64006dd1933d
Zoey Child <imperialoctopus@gmail.com>
2023-01-09 00:29:31 +0000

    Add view card button to export page

--- lib/routes/export_page.dart
+++ lib/routes/export_page.dart
@@ -1,7 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/board_state.dart';
+import '../router.dart';
 import '../services/secret_code_service.dart';
 
 class ExportPage extends StatelessWidget {
@@ -24,6 +26,14 @@ class ExportPage extends StatelessWidget {
                   boardState.cards.map((card) => card.affiliation).toList()),
               style: Theme.of(context).textTheme.displayMedium,
             ),
+            const SizedBox(height: 32),
+            OutlinedButton(
+              onPressed: () => context.router.push(SpymasterRoute(
+                  spymasterData: boardState.cards
+                      .map((card) => card.affiliation)
+                      .toList())),
+              child: const Text('view card'),
+            )
           ],
         ),
       ),
